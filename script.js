2b4a0f5a5eb1bb44ad6a2a14369244db13fce530
Dipesh <58142361+Dipesh-dev@users.noreply.github.com>
2020-08-08 09:55:41 -0700

    Hamburger menu added

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 // Selecting the menu button
-const menu = document.querySelector(".menu-btn");
+const menu = document.querySelector(".hamburger");
 menu.addEventListener("click", () => {
 
     if(document.querySelector(".navs").classList.contains("none")) {
         document.querySelector(".navs").classList.remove("none")
     } else {
         document.querySelector(".navs").classList.add("none")
+    }
+})
+
+const btn = document.querySelector(".hamburger");
+let menuOpen = false;
+btn.addEventListener("click", e => {
+    if(!menuOpen) {
+        document.querySelector(".line").classList.add("open");
+        menuOpen = true;
+    } else {
+        document.querySelector(".line").classList.remove("open");
+        menuOpen = false;
     }
 })
